136e884621bc4ba8b6483293d229421a3af4c86f
Josip Antoliš <josip.antolis@protonmail.com>
2020-07-15 01:02:42 +0200

    2nd attempt: provide configured NettyReactiveWebServerFactory bean

--- src/main/java/io/github/antolius/sgc/examples/query/length/App.java
+++ src/main/java/io/github/antolius/sgc/examples/query/length/App.java
@@ -3,6 +3,8 @@ package io.github.antolius.sgc.examples.query.length;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +24,20 @@ public class App {
         return builder.routes()
                 .route(r -> r.path("/foo/bars").uri(backendUrl))
                 .build();
+    }
+
+    @Bean
+    public NettyReactiveWebServerFactory nettyFactory(ServerProperties serverProperties) {
+        var maxInBytes = (int) serverProperties.getMaxHttpHeaderSize().toBytes();
+        var factory = new NettyReactiveWebServerFactory();
+        factory.addServerCustomizers(
+                server -> server.httpRequestDecoder(
+                        reqDecoder -> reqDecoder
+                                .maxInitialLineLength(maxInBytes)
+                                .maxHeaderSize(maxInBytes)
+                )
+        );
+        return factory;
     }
 
 }
